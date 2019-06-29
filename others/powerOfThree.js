3627cfe676abe16f9a23d2193e8df065ccdcac35
Peter Lin <szul@uw.edu>
2019-06-29 17:02:26 +0800

    update

---
+++ others/powerOfThree.js
@@ -0,0 +1,8 @@
+// Leetcode #326
+var isPowerOfThree = function(n) {
+  if (n / 3 === 1 || n === 1) return true;
+  if (n % 3 !== 0 || n === 0) return false;
+  return isPowerOfThree(n / 3);
+};
+const res = isPowerOfThree(0);
+console.log('res', res);
---
+++ others/reverseString.js
@@ -0,0 +1,16 @@
+// Leetcode 344
+var reverseString = function(s) {
+  let lastIndex = s.length - 1;
+  for (let i = 0; i < s.length; i++) {
+    const lastEl = s[lastIndex];
+    s[lastIndex] = s[i];
+    s[i] = lastEl;
+    if (lastIndex === i) return s;
+    if (lastIndex - i === 1) {
+      return s;
+    }
+    lastIndex--;
+  }
+};
+const res = reverseString(['H', 'a', 'n', 'n', 'a', 'h']);
+console.log('res', res);
