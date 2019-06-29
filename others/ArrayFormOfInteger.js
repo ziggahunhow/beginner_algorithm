2a2a023e1e0b731981817e7cb070571cd3e797e2
Peter Lin <szul@uw.edu>
2019-06-29 16:00:40 +0800

    new solutions

---
+++ others/ArrayFormOfInteger.js
@@ -0,0 +1,30 @@
+// Leetcode #989
+const addToArrayForm = function(A, K) {
+  let carry = 0;
+  for (let i = A.length - 1; i >= 0; i--) {
+    A[i] = A[i] + (K % 10);
+    if (carry !== 0) A[i] = A[i] + carry;
+    if (A[i] > 9) {
+      A[i] = A[i] - 10;
+      carry = 1;
+    } else carry = 0;
+    K = Math.floor(K / 10);
+  }
+  if (K !== 0) {
+    if (carry !== 0) {
+      K = K + carry;
+      carry = 0;
+    }
+    const b = Array.from(String(K), Number);
+    A = b.concat(A);
+  }
+  if (carry !== 0) {
+    A = [1].concat(A);
+  }
+  return A;
+};
+
+const res = addToArrayForm([6], 809);
+console.log('res', res);
+
+// A = [9,9,9,9,9,9,9,9,9,9], K = 1
---
+++ others/commonChar.js
@@ -0,0 +1,39 @@
+// Leetcode #1002
+var commonChars = function(A) {
+  let res = [];
+  let shortest = '';
+  let hashT = {};
+  // find shortest word
+  for (let i = 0; i < A.length; i++) {
+    if (i === 0) shortest = A[i];
+    if (A[i].length < shortest.length) shortest = A[i];
+  }
+  // shortest word's hash Table count
+  for (let letter = 0; letter < shortest.length; letter++) {
+    const el = shortest[letter];
+    hashT = {
+      ...hashT,
+      [el]: hashT[el] ? hashT[el] + 1 : 1
+    };
+  }
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === shortest) continue;
+    let count = 0;
+    for (let letter = 0; letter < A[i].length; letter++) {
+      console.log('A[i]', A[i][letter]);
+      // console.log('count', count);
+      if (Object.keys(hashT).includes(A[i][letter])) {
+        // console.log('A[i]', A[i]);
+        // console.log('A[i][letter]', A[i][letter]);
+        count++;
+        if (count <= hashT[A[i][letter]]) {
+          hashT[A[i][letter]] = count;
+        }
+      }
+      count = 0;
+    }
+  }
+  console.log('hashT', hashT);
+};
+
+commonChars(['cool', 'loock', 'cook']);
